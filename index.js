2a8e60b78eb14574b0f06a803b9644c827efcdd1
ajcolombini <ajcolombini@gmail.com>
2018-04-20 14:52:11 -0300

    Add files via upload
    Versão ajustada (beta)

--- index.js
+++ index.js
@@ -12,39 +12,59 @@ var tradeApi = new MercadoBitcoinTrade({
     pin: process.env.PIN 
 })
 
-setInterval(() => 
+
+
+
+//setInterval(() => 
    //infoApi.ticker((tick) => console.log(tick.ticker)),
-   getBalance('BCH', 0, false, (balance) => {console.log(balance)}),
-   process.env.CRAWLER_INTERVAL
-)
+   //getBalance('BCH', 0, false, (balance) => {console.log(balance)})
+		//,process.env.CRAWLER_INTERVAL
+//)
+
+// function timeConverter(UNIX_timestamp){
+  // var a = new Date(UNIX_timestamp * 1000);
+  // //var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+  // var year = a.getFullYear();
+  // var month = a.getMonth() + 1 ;  //months[a.getMonth()];
+  // var date = a.getDate();
+  // var hour = a.getHours();
+  // var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+  // var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
+  // var time = date + '/' + month + '/' + year + ' ' + hour + ':' + min + ':' + sec ;
+  // return time;
+// }
 
 function getBalance(coin, price, isBuying, callback){
     price = parseFloat(price)
-    coin = isBuying ? 'BRL' : coin.toLowerCase()
-
+    coin = isBuying ? 'brl' : coin.toLowerCase()
+		
     tradeApi.getAccountInfo((response_data) => {
-        var balance = parseFloat(response_data.balance[coin].available).toFixed(5)
-		balance = parseFloat(balance)
+
+		var ret = JSON.parse(JSON.stringify(response_data));
+
+        var balance = parseFloat(eval('ret.balance.' + coin + '.available')).toFixed(5); //isBuying ? parseFloat(eval('ret.balance.brl.available')).toFixed(5) : parseFloat(eval('ret.balance.' + coin + '.available')).toFixed(5);
+		balance = parseFloat(balance);
+		
         if(isBuying && balance < 50) 
-			return console.log('Sem saldo disponível em BRL para comprar!')
+			return console.log('Sem saldo disponível para comprar!  BRL ' + balance)
         
+		var finalBalance = balance;
+		
         if(isBuying) 
 		{
-			balance = parseFloat((balance / price).tofixed(5))
+			balance = parseFloat((balance / price).toFixed(5))
 			
 			//tira a diferença que se ganha no arredondamento
-			var finalBalance = (parseFloat(balance) - 0.00001);
-			console.log(`Saldo disponível de ${coin.toUpperCase()}: ${finalBalance}`)
-			callback(finalBalance);
+			finalBalance = (parseFloat(balance) - 0.00001);
 		} 
-			
-		console.log(`Saldo total disponível de ${coin.toUpperCase()}: ${balance}`);
+		console.log(`Saldo disponível de ${coin.toUpperCase()}: ${finalBalance}`);
 		
+		callback(finalBalance);
     }, 
     (data) => console.log(data))
 }
 
-function tradeCoin(coin, qty, price) {
+function tradeCoin(coin, priceToBuy, priceToSell, useStopProfit) {
 	
 	var infoApi = new MercadoBitcoin({ currency: coin })
 	var tradeApi = new MercadoBitcoinTrade({ 
@@ -53,28 +73,76 @@ function tradeCoin(coin, qty, price) {
 		secret: process.env.SECRET, 
 		pin: process.env.PIN 
 	})
+	console.log('Moeda: ' + coin);
+	console.log('Preço desejado para compra: ' + priceToBuy);
+	console.log('Preço desejado para venda : ' + priceToSell);
+	console.log('Usando Stop Profit? : ' + (useStopProfit ? 'Sim': 'Não'));
+	//console.log( useStopProfit ? 'Sim': 'Não');
 	
 	setInterval(() => 
 	    infoApi.ticker((response) => {
-		    console.log(response.ticker)
-		    if(response.ticker.sell <= price) //Se valor de venda menor ou igual ao preço definido para compra
+			
+		    //console.log('Comprar? ' + (response.ticker.sell <= priceToBuy ? 'sim':'não'));
+			console.log('');
+			
+		    if(response.ticker.sell <= priceToBuy) //Se valor de venda menor ou igual ao preço definido para compra
 		    {
-				getBalance('BRL', response.ticker.sell, true, (balance) => {
+			
+				 getBalance(coin, response.ticker.sell, true, (balance) => {
 					 var qtyToBuy = balance / 2; //Metade do saldo disponivel
-					 tradeApi.placeBuyOrder(qtyToBuy, response.ticker.sell, 
-						 (data) => {
-							 console.log('Ordem de compra de ' + qtyToBuy + ' ' + coin + ',  por '  + price + ', inserida no livro. ' + data)
-							 //operando em STOP com lucro de <env.PROFITABILITY>%
-							 tradeApi.placeSellOrder(data.quantity, response.ticker.sell * parseFloat(process.env.PROFITABILITY), 
-								 (data) => console.log('Ordem de venda de ' + data.quantity + '  ' + coin + ',  por '  + response.ticker.sell + ',  inserida no livro. ' + data),
-								 (data) => console.log('Erro ao inserir ordem de venda  de ' + data.quantity + '  ' + coin + ',  por '  + response.ticker.sell + ' >> '  + data))
-						 },
-						 (data) => console.log('Erro ao inserir ordem de compra  de ' + qtyToBuy + '  ' + coin + ',  por '  + response.ticker.sell + ' >> ' + data));
-				})
+					 
+					 //teste
+					 console.log('Ordem de compra de ' + qtyToBuy + ' ' + coin + ',  por '  + priceToBuy + ', inserida no livro.');
+					 
+					 // // tradeApi.placeBuyOrder(qtyToBuy, response.ticker.sell, 
+						 // // (data) => {
+							 //console.log('Ordem de compra de ' + qtyToBuy + ' ' + coin + ',  por '  + priceToBuy + ', inserida no livro. >> ' + data)
+							
+							 if(useStopProfit) {							
+							 // ////operando em STOP com lucro de <env.PROFITABILITY>%
+							 // // tradeApi.placeSellOrder(data.quantity, response.ticker.sell * parseFloat(process.env.PROFITABILITY), 
+								 // //(data) => console.log('Ordem de venda de ' + data.quantity + '  ' + coin + ',  por '  + response.ticker.sell * parseFloat(process.env.PROFITABILITY + ',  inserida no livro. >> ' + data),
+								 // // (data) => console.log('Erro ao inserir ordem de venda  de ' + data.quantity + '  ' + coin + ',  por '  + response.ticker.sell + ' >> '  + data))
+							 
+								// //teste
+								 console.log('Ordem de venda de ' + qtyToBuy + '  ' + coin + ',  por '  + response.ticker.sell * parseFloat(process.env.PROFITABILITY) + ',  inserida no livro.');
+							 }								
+						 // // },
+						 // // (data) => console.log('Erro ao inserir ordem de compra  de ' + qtyToBuy + '  ' + coin + ',  por '  + response.ticker.sell + ' >> ' + data));
+				 });
 			}
 			else
-				console.log('Preço muito alto (' + response.ticker.sell + '), vamos esperar pra comprar depois...' + data)
+				console.log('Preço muito alto (' + response.ticker.sell + '), vamos comprar depois...');
+			
+			if(!useStopProfit)
+			{
+				//Hora de vender?
+				if( response.ticker.buy >= priceToSell || (response.ticker.buy >= (response.ticker.high * 0.99)) ) 
+				{
+					getBalance(coin, response.ticker.buy, false, (balance) => {
+						 var qtyToSell = balance; 
+						 
+						 
+						 if(balance > 0){
+							// tradeApi.placeSellOrder(qtyToSell, response.ticker.last, 
+							// (data) => {
+							  // (data) => console.log('Ordem de venda de ' + qtyToSell + '  ' + coin + ',  por '  + response.ticker.last + ',  inserida no livro. ' + data),
+						 	  // (data) => console.log('Erro ao inserir ordem de venda  de ' + qtyToSell + '  ' + coin + ',  por '  + response.ticker.last + ' >> '  + data))
+							// }
+							
+							//teste
+							console.log('Ordem de venda de ' + qtyToSell + '  ' + coin + ',  por '  + response.ticker.buy + ',  inserida no livro. ');
+						 }
+					});
+				}
+				else
+					console.log('Ainda não é hora de vender ( actual:' + response.ticker.buy + ' / higher:' +  response.ticker.high + ')...');
+			}
 	    }),
-	    process.env.CRAWLER_INTERVAL
-	)
+	    process.env.CRAWLER_INTERVAL);
 }
+
+
+//chama funcao trader
+tradeCoin('BCH', '3500', '3899', false);
+
